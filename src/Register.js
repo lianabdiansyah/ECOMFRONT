231054aa913a9eabaf159ea7db3da5918c6b56e5
liancmd <lianabdiansyahtanjung@gmail.com>
2021-09-18 02:45:06 +0700

    menambahkan form di halaman register

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,37 @@
 function Register() {
   return (
-    <div>
-      <h1>Halaman Register</h1>
+    <div className="container d-flex justify-content-center">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="text-center">Halaman Register</h1>
+          <form>
+            <div className="mb-3">
+              <label for="nama" className="form-label">
+                Nama
+              </label>
+              <input type="text" className="form-control" id="nama" />
+            </div>
+            
+            <div className="mb-3">
+              <label for="email" className="form-label">
+                Email
+              </label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            
+            <div className="mb-3">
+              <label for="password" className="form-label">
+                Password
+              </label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
