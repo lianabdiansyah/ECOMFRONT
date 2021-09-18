43afe818d94a29cf900679330de0eef46d4c23c6
liancmd <lianabdiansyahtanjung@gmail.com>
2021-09-18 22:45:32 +0700

    menambahkan fungsi form state

--- src/Register.js
+++ src/Register.js
@@ -1,35 +1,70 @@
+import { useState } from "react/cjs/react.development";
+
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit=()=> {
+    let item = { name, email, password };
+    console.warn(item);
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <div className="container d-flex justify-content-center">
       <div className="row">
         <div className="col-md-12">
           <h1 className="text-center">Halaman Register</h1>
-          <form>
+          
             <div className="mb-3">
-              <label for="nama" className="form-label">
+              <label htmlFor="nama" className="form-label">
                 Nama
               </label>
-              <input type="text" className="form-control" id="nama" />
-            </div>
-            
-            <div className="mb-3">
-              <label for="email" className="form-label">
-                Email
-              </label>
-              <input type="email" className="form-control" id="email" />
-            </div>
-            
-            <div className="mb-3">
-              <label for="password" className="form-label">
-                Password
-              </label>
-              <input type="password" className="form-control" id="password" />
+              <input
+                type="text"
+                className="form-control"
+                id="nama"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
 
-            <button type="submit" className="btn btn-primary">
-              Submit
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button onClick={handleSubmit}
+              type="submit"
+              className="btn btn-primary"
+            >
+              Kirim
             </button>
-          </form>
+         
         </div>
       </div>
     </div>
